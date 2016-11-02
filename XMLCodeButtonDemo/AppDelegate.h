f693a1401d96e5f7238246514da1d98a5bd56d45
zhangjiahuan <573976948@qq.com>
2016-11-02 14:38:06 +0800

    Initial Commit

---
+++ XMLCodeButtonDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMLCodeButtonDemo
+//
+//  Created by aurorac on 16/11/2.
+//  Copyright © 2016年 xiaomaolv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMLCodeButtonDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMLCodeButtonDemo
+//
+//  Created by aurorac on 16/11/2.
+//  Copyright © 2016年 xiaomaolv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
